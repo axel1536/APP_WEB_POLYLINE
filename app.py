073b504f3f28d7a9abd1b0171c932d3bff838563
axel1536 <axeljeremy200@gmail.com>
2026-01-14 17:58:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -469,7 +469,7 @@ if st.session_state.get("pagina") == "caja":
     if st.button("Volver"):
         st.session_state["pagina"] = None
         st.rerun()
-    def mostrar_caja_chica()
+    mostrar_caja_chica()
     st.stop()
 
 # =========================
